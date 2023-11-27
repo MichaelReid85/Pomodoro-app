da687bdd4366ad496202ee99c51ba6b287def4b7
MichaelReid85 <Mike1109r@yahoo.com>
2023-11-27 13:10:24 -0800

    finished project

--- main.py
+++ main.py
@@ -9,34 +9,42 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-REPS = 0
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 
-# def restart_timer():
-#     canvas.itemconfig(timer_text, text=count)
-#     if count > 0:
-#         window.after(1000, count_down, count - 1)
-#     count_down(25)
+def reset_timer():
+    window.after_cancel(timer)
+    title.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    check.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start_timer():
-    global REPS
-    REPS += 1
+    global reps
+    reps += 1
 
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    if REPS % 8 == 0:
+    if reps % 8 == 0:
         count_down(long_break_sec)
-    elif REPS % 2 == 0:
+        title.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
         count_down(short_break_sec)
+        title.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
+        title.config(text="Work", fg=GREEN)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
@@ -51,9 +59,14 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
-
-
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        for _ in range(math.floor(reps / 2)):
+            marks += "🗸"
+            check.config(text=marks)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -75,10 +88,10 @@ canvas.grid(column=1, row=1)
 start = Button(text="Start", highlightthickness=0, command=start_timer)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset", highlightthickness=0)
+reset = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset.grid(column=2, row=2)
 
-check = Label(text="🗸", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+check = Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
 check.grid(column=1, row=3)
 
 window.mainloop()
